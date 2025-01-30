8b8b9f8a3c0f16a0f363a8bae911c47244980b9e
Christian Gasca <136762632+perseus369@users.noreply.github.com>
2025-01-30 17:15:09 +0000

    Added paragraph element to the HTML, added arrays containing the parts of the excuses, created a function to randomize the items in the arrays, printed the concatenations of all the parts of the excuse on the paragraph.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse = document.querySelector("#excuse");
+  const getRandomInt = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+  excuse.innerHTML =
+    who[getRandomInt(who)] +
+    " " +
+    action[getRandomInt(action)] +
+    " " +
+    what[getRandomInt(what)] +
+    " " +
+    when[getRandomInt(when)];
 };
